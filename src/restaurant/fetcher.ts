0013b125d9b61b0f9ad4aa7116c27b5e1ec74844
sotaroNishioka <nishiokanandesuka@gmail.com>
2024-01-21 19:11:39 +0900

    レストランページ別fetchまで追加

--- src/restaurant/fetcher.ts
+++ src/restaurant/fetcher.ts
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom'
 import fetch from 'node-fetch'
 import { retry } from '../utils/retry'
 
-export const getRestaurantDom = async (arg: { cityUrl: string, miniorCategoryCode: string }): Promise<JSDOM> => {
+export const getRestaurantCountDom = async (arg: { cityUrl: string, miniorCategoryCode: string }): Promise<JSDOM> => {
   let response
   try {
     response = await retry(() => fetch(`${arg.cityUrl}/rstLst/${arg.miniorCategoryCode}/`), 3)
@@ -12,5 +12,18 @@ export const getRestaurantDom = async (arg: { cityUrl: string, miniorCategoryCod
   }
   const body = await response.text()
   const dom = new JSDOM(body)
+  return dom
+}
+
+export const getRestaurantPageDom = async (arg: { cityUrl: string, miniorCategoryCode: string, page: number }) => {
+  let response
+  try {
+    response = await retry(() => fetch(`${arg.cityUrl}/rstLst/${arg.miniorCategoryCode}/${arg.page}/`), 3)
+  } catch (err) {
+    console.error(err)
+    throw new Error('restaurant fetch error')
+  }
+  const bory = await response.text()
+  const dom = new JSDOM(bory)
   return dom
 }
--- src/restaurant/index.ts
+++ src/restaurant/index.ts
@@ -1,6 +1,6 @@
 import { getCities } from '../city/db'
 import { getAllMiniorCategory } from '../category/db'
-import { getRestaurantDom } from './fetcher'
+import { getRestaurantCountDom, getRestaurantPageDom } from './fetcher'
 import { getRestaurantCount } from './dom'
 
 export const asyncUpdateRestaurant = async (): Promise<void> => {
@@ -10,9 +10,14 @@ export const asyncUpdateRestaurant = async (): Promise<void> => {
 
   for (const city of cities) {
     for (const category of categories) {
-      const dom = await getRestaurantDom({ cityUrl: city.url, miniorCategoryCode: category.code })
-      const count = getRestaurantCount(dom)
+      const countDom = await getRestaurantCountDom({ cityUrl: city.url, miniorCategoryCode: category.code })
+      const count = getRestaurantCount(countDom)
       const pageArr = Array.from({ length: Math.ceil(count / 20) }, (_, i) => i + 1)
+      for (const page of pageArr) {
+        const pageDom = getRestaurantPageDom({ cityUrl: city.url, miniorCategoryCode: category.code, page })
+        console.log(pageDom)
+      }
+
       console.log(`city: ${city.name}, category: ${category.name}, count: ${count}, pageArr: ${pageArr.length}`)
     }
   }
